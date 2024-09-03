e45b2934f0e9d2012786e6b4c06d97657693fc0f
Rocío <rplatini@fi.uba.ar>
2024-09-02 22:48:00 -0300

    More fixes

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 MESSAGE="testing my server"
 NETWORK_NAME="tp0_testing_net"
-IMAGE_NAME="example"
+IMAGE_NAME="busybox"
 
-docker build -t "$IMAGE_NAME" .
-RESPONSE=$(docker run --rm --network=$NETWORK_NAME $IMAGE_NAME)
+# docker build -t "$IMAGE_NAME" .
+RESPONSE=$(docker run --rm --network=$NETWORK_NAME $IMAGE_NAME sh -c "echo testing my server | nc -w 3 server 12345")
 
 if [ "$RESPONSE" == "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
