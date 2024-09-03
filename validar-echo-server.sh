293438717a1a8bdb05b94c4f99ae6a16cccc0fe5
Rocío <rplatini@fi.uba.ar>
2024-09-02 23:21:26 -0300

    Refactor script

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -5,10 +5,5 @@ IMAGE_NAME="image"
 
 docker build -t "$IMAGE_NAME" .
 RESPONSE=$(docker run --rm --network=$NETWORK_NAME $IMAGE_NAME)
-python3 -c "print('action: test_echo_server | result: success' if '$RESPONSE' == '$MESSAGE' else 'action: test_echo_server | result: fail')"
 
-# if [ "$RESPONSE" == "$MESSAGE" ]; then
-#     echo "action: test_echo_server | result: success"
-# else
-#     echo "action: test_echo_server | result: fail"
-# fi
+python3 -c "print('action: test_echo_server | result: success' if '$RESPONSE' == '$MESSAGE' else 'action: test_echo_server | result: fail')"
