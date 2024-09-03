eb9e32b16bd9d24e29dbd529f4abf3638d4448b9
Rocío <rplatini@fi.uba.ar>
2024-09-02 23:12:14 -0300

    Test

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -5,9 +5,10 @@ IMAGE_NAME="busybox"
 
 # docker build -t "$IMAGE_NAME" .
 RESPONSE=$(docker run --rm --network=$NETWORK_NAME $IMAGE_NAME sh -c "echo testing my server | nc -w 3 server 12345")
+python3 -c "print('action: test_echo_server | result: success' if '$RESPONSE' == '$MESSAGE' else 'action: test_echo_server | result: fail')"
 
-if [ "$RESPONSE" == "$MESSAGE" ]; then
-    echo "action: test_echo_server | result: success"
-else
-    echo "action: test_echo_server | result: fail"
-fi
+# if [ "$RESPONSE" == "$MESSAGE" ]; then
+#     echo "action: test_echo_server | result: success"
+# else
+#     echo "action: test_echo_server | result: fail"
+# fi
