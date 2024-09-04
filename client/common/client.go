1ea180f88f799a341cffb220a1df662d0c7e1912
Rocío <rplatini@fi.uba.ar>
2024-09-04 09:46:57 -0300

    Refactors

--- client/common/client.go
+++ client/common/client.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
@@ -14,7 +15,9 @@ import (
 
 var log = logging.MustGetLogger("log")
 
-const BATCH_SIZE = 8000
+const AGENCY_FILE = "./agency.csv"
+const DELIMITER = ","
+const END_OF_BET = "\n"
 
 // ClientConfig Configuration used by the client
 type ClientConfig struct {
@@ -30,6 +33,7 @@ type Client struct {
 	config ClientConfig
 	conn  net.Conn
 	running bool
+	file *os.File
 }
 
 // NewClient Initializes a new client receiving the configuration
@@ -59,13 +63,24 @@ func (c *Client) createClientSocket() error {
 	return nil
 }
 
+// Converts a bet to a byte array
+func (c * Client) serialize_bet(bet []string) []byte {
+	bet = append([]string{string(c.config.ID)}, bet...)
+	data := strings.Join(bet, DELIMITER)
+	data += END_OF_BET
+	
+	log.Debugf("action: serialize_bet | client_id: %v | data: %v", c.config.ID, data)
+
+	return []byte(data)
+}
+
+// sendBets Sends a batch of bets to the server. The batch size is not larger than 8kb
 func (c * Client) sendBets(reader *csv.Reader, messageHandler *MessageHandler) (bool, error) {
-	batchMsg := []byte{}
 	eofReached := false
+	batchMsg := []byte{}
 
 	for i := 0; i < c.config.BatchAmount; i++ {
 		line, err := reader.Read()
-	
 		if err != nil {
 			if err == io.EOF {
 				eofReached = true
@@ -73,20 +88,11 @@ func (c * Client) sendBets(reader *csv.Reader, messageHandler *MessageHandler) (
 			}
 			return eofReached, err
 		}
-	
-		bet := NewBet(
-			c.config.ID,
-			line[0],
-			line[1],
-			line[2],
-			line[3],
-			line[4],
-		)
 
-		batchMsg = append(batchMsg, bet.serialize()...)
+		batchMsg = append(batchMsg, c.serialize_bet(line)...)
 	}
 
-	err := messageHandler.sendMessage([]byte(batchMsg), eofReached)
+	err := messageHandler.sendMessage(batchMsg, eofReached)
 
 	if err != nil {
 		return eofReached, err
@@ -95,9 +101,21 @@ func (c * Client) sendBets(reader *csv.Reader, messageHandler *MessageHandler) (
 	return eofReached, nil
 }
 
+// getBetsReader Returns a csv.Reader to read the bets file
+func (c *Client) getBetsReader() *csv.Reader {
+	file, err := os.OpenFile(AGENCY_FILE, os.O_CREATE|os.O_RDONLY, 0777)
+    if err != nil {
+        log.Errorf("Error opening file: %v", err)
+		return nil
+    }
 
-// StartClientLoop Send messages to the client until some time threshold is met
-func (c *Client) StartClientLoop(reader *csv.Reader) {
+	c.file = file
+	reader := csv.NewReader(file)
+	return reader
+}
+
+// catchSignal Catches the signal SIGTERM to exit gracefully
+func (c *Client) catchSignal() {
 	sigChannel := make(chan os.Signal, 1)
    	signal.Notify(sigChannel, os.Interrupt, syscall.SIGTERM)
 
@@ -107,13 +125,19 @@ func (c *Client) StartClientLoop(reader *csv.Reader) {
 			c.signalHandler(s)
 		} 
 	}()
-	
-	if !c.running {
-		log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
+}
+
+// StartClientLoop Send messages batches of bets to the server
+func (c *Client) StartClientLoop() {
+	c.catchSignal()
+
+	if err := c.createClientSocket(); err != nil {
 		return
 	}
 
-	if err := c.createClientSocket(); err != nil {
+	reader := c.getBetsReader()
+	if reader == nil {
+		c.conn.Close()
 		return
 	}
 	messageHandler := &MessageHandler{conn: c.conn}
@@ -129,30 +153,35 @@ func (c *Client) StartClientLoop(reader *csv.Reader) {
 				err,
 			)
 			c.conn.Close()
+			c.file.Close()
 			return
 		}
 	}
 
-	_, err := messageHandler.receiveMessage()
-	c.conn.Close()
-
-	if err != nil {
+	if _, err := messageHandler.receiveMessage() ; err != nil {
 		log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
 			c.config.ID,
 			err,
 		)
-		return
+	} else {
+		log.Infof("action: apuestas_enviadas | result: success | client_id: %v", c.config.ID)
 	}
 
-	log.Infof("action: apuestas_enviadas | result: success | client_id: %v", c.config.ID)
+	c.conn.Close()
+	c.file.Close()
 }
 
 func (c *Client) signalHandler(signal os.Signal) {
 	c.running = false
 
 	if c.conn != nil {
-		log.Debugf("action: close_connections | result: in_progress | client_id: %v", c.config.ID)
 		c.conn.Close()
-		log.Debugf("action: exit | result: success | client_id: %v", c.config.ID)
+		log.Debugf("action: close_connections | result: success | client_id: %v", c.config.ID)
 	}
+
+	if c.file != nil {
+		c.file.Close()
+		log.Debugf("action: close_file | result: sucess | client_id: %v", c.config.ID)
+	}
+	log.Debugf("action: exit | result: success | client_id: %v", c.config.ID)
 }
